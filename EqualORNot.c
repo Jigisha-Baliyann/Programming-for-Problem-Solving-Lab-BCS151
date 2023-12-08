d61b95da54e69d469eadb8198269e68fc6974e00
Jigisha Baliyann <115288853+Jigisha-Baliyann@users.noreply.github.com>
2023-12-08 22:56:03 +0530

    Create EqualORNot.c

---
+++ EqualORNot.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+ int a,b;
+ printf("Enter a=");
+ scanf("%d",&a);
+ printf("Enter b=");
+ scanf("%d",&b);
+
+ if(a==b)
+ {
+ printf("\na and b are equal.");
+ }
+ else
+ {
+ printf("\na and b are not equal.");
+ }
+}
+
+// OUTPUT:
+
+/*
+Enter a=10
+Enter b=12
+a and b are not equal.
+*/
