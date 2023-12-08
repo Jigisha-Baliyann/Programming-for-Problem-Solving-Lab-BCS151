b20ced8d20aef76160e14f5520c80ce3453e7ca8
Jigisha Baliyann <115288853+Jigisha-Baliyann@users.noreply.github.com>
2023-12-08 22:58:45 +0530

    Create EvenOROdd.c

---
+++ EvenOROdd.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+ int n;
+ printf("Enter a number=");
+ scanf("%d",&n);
+
+ if(n%2==0)
+ {
+ printf("\nn is even number.");
+ }
+ else
+ {
+ printf("\nn is odd number.");
+ }
+}
+
+// OUTPUT:
+
+/*
+Enter a number=15
+n is odd number. 
+*/
