b115fa24e8200ceb581f5ce6c736310671957fc1
Jigisha Baliyann <115288853+Jigisha-Baliyann@users.noreply.github.com>
2023-12-08 22:54:15 +0530

    Create TemperatureConvertor.c

---
+++ TemperatureConvertor.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ float c,f;
+ printf("Enter Centigrade=");
+ scanf("%f",&c);
+ f=(9*c)/5+32;
+ printf("Fahrenheit=%f",f);
+}
+
+// OUTPUT:
+
+/*
+Enter Centigrade=36
+Fahrenheit=96.800003
+*/
